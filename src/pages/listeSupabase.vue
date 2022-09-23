24b01aa19b72ac2895eab70cc23cb249cd86d54e
LudoLaFayette <ludovicfaye89@gmail.com>
2022-09-23 08:11:31 +0200

    ajout supabase + page

---
+++ src/pages/listeSupabase.vue
@@ -0,0 +1,15 @@
+<script setup>
+// const res = await fetch("/public/jSon/maisons.json");
+// const maisons = await res.json();
+
+import Card from "../components/card.vue";
+import { supabase } from "../supabase";
+console.log("supabase :", supabase); // pour vérifier et "garder" supabase dans le code
+const maisons = []; // à remplacer par l'appel à Supabase
+</script>
+            
+<template>
+    <Card v-for="maison in maisons" :key="maison.id" v-bind="maison"/>       
+</template>
+// ok
+        
