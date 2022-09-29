3ad715bbd8ba15e16c9ab5bba7cf2f57d932f52a
LudoLaFayette <ludovicfaye89@gmail.com>
2022-09-29 11:36:39 +0200

    review visuel card

--- src/components/card.vue
+++ src/components/card.vue
@@ -5,7 +5,7 @@ import Dimension from "@/components/Dimension.vue";
 import Bed from "@/components/Bed.vue";
 defineProps({
   nom: String,
-  adress:String,
+  adress: String,
   prix: Number,
   favori: Boolean,
   image: String, // les images sont simplement l'URL absolue (depuis la racine, débute par '/' )
@@ -17,31 +17,25 @@ defineProps({
 
 <template>
   <figure>
-    <!-- Pour changer la valeur d'un attribut on utilise un "binding" :
-    https://vuejs.org/guide/essentials/template-syntax.html#attribute-bindings 
-    Ici on affecte à l'attribut 'src' la 'props' "image" -->
-    <img class="w-full h-48 object-cover" :src="image" alt="" />
+    
+    <img class="w-full h-48 object-cover mt-5 rounded-t-lg" :src="image" alt="" />
     <figcaption
-      class="relative flex flex-col border-2 border-t-0 border-indigo-100"
-    >
-      <!-- Pour changer une classe CSS en fonction d'un booléen : 
-      https://vuejs.org/guide/essentials/class-and-style.html#binding-html-classes -->
-      <heart :class="{ 'fill-red-300': favori }" />
-
-      <!-- Pour afficher du contenu textuel, simplement utiliser l'interpolation par double accolades (qui contiendront le JS dont l'interpretation donne le résultat affiché). -->
-      <div class="text-2xl font-bold text-indigo-500 pr-1">${{ prix }}</div>
-
+      class="relative flex flex-col border-2 border-t-0 px-5 py-7 border-indigo-100" >    
       <h3 class="text-2xl text-gray-900">{{ nom }}</h3>
-      <h3 class="text-2xl text-gray-900">{{ adress }}</h3>
-
-      <hr class="border-indigo-100 border-t-2 my-4" />
-      <div class="flex gap-5">
-      <div><bathroom class="inline-block pr-1" />{{ nbrSDB }}</div>
-      <div><bed class="inline-block pr-1" />{{ nbrBeds }}</div>
-      <div><dimension class="inline-block pr-1" />{{ dimension }}</div>
+      <address class="my-2 not-italic text-gray-500">{{ adress }}</address>
+      <hr class="my-2 border-t-2 border-indigo-100" />
+      <div class="-order-1 flex items-center">
+        <div class="text-2xl text-indigo-500">${{ prix.toLocaleString("en-US") }}</div>
+        <div class="text text-gray-900">/months</div>
       </div>
-
-      
+      <div class="flex justify-between text-sm">
+        <div><bed class="inline-block pr-1 align-top" /> {{ nbrBeds }} beds</div>
+        <div><bathroom class="inline-block pr-1 align-top" /> {{ nbrSDB }} bathrooms</div>
+        <div><dimension class="inline-block pr-1 align-top" /> {{ dimension }}</div>
+      </div>
+      <div class="absolute top-7 right-5 grid h-12 w-12 place-items-center rounded-full border-2 border-indigo-100">
+        <heart :class="{ 'fill-red-400': true }" />
+      </div>    
     </figcaption>
   </figure>
 </template>
